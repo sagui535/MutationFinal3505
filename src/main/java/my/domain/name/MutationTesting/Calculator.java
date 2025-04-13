07a2523fd81f5fedc827a743091249537e868419
sagui535 <113629950+sagui535@users.noreply.github.com>
2025-04-13 00:39:46 -0600

    all ai gen mutations in calc class

--- src/main/java/my/domain/name/MutationTesting/Calculator.java
+++ src/main/java/my/domain/name/MutationTesting/Calculator.java
@@ -1,37 +1,63 @@
 package my.domain.name.MutationTesting;
-
 public class Calculator {
-	
-    public int add(int a, int b) {
-        return a + b;
-    }
-    
-    public int subtract(int a, int b) {
-        return a - b;
-    }
-    
-    public boolean isPositive(int number) {
-        return number > 0;
-    }
-    
-    public int max(int a, int b) {
-        if (a > b) {
-            return a;
-        }
-        else {
-            return b;
-        }
-    }
-    
-    public int multiply(int a, int b) {
-        return a * b;
-    }
-    
-    public int divide(int a, int b) {
-        if (b == 0) {
-            throw new IllegalArgumentException("Cannot divide by zero");
-        }
-        return a / b;
-    }
-
+  
+   // Mutation: Changed '+' to '-'
+   public int add(int a, int b) {
+       return a - b;
+   }
+  
+   // Mutation: Changed '-' to '+'
+   public int subtract(int a, int b) {
+       return a + b;
+   }
+  
+   // Mutation: Changed '>' to '>='
+   public boolean isPositive(int number) {
+       return number >= 0;
+   }
+  
+   // Mutation: Changed '>' to '<'
+   public int max(int a, int b) {
+       if (a < b) {
+           return a;
+       }
+       else {
+           return b;
+       }
+   }
+  
+   // Mutation: Always returns 0
+   public int multiply(int a, int b) {
+       return 0;
+   }
+  
+   // Mutation: Removed zero-check
+   public int divide(int a, int b) {
+       return a / b;
+   }
+   // Additional mutations that weren't in original class
+   // but were suggested in the mutations list:
+  
+   // Mutation: Returns 0 on division by zero
+   public int divideSafe(int a, int b) {
+       if (b == 0) {
+           return 0;
+       }
+       return a / b;
+   }
+  
+   // Mutation: Changed to check for negativity
+   public boolean isNegative(int number) {
+       return number < 0;
+   }
+  
+   // Mutation: Always returns first argument
+   public int maxAlwaysA(int a, int b) {
+       return a;
+   }
+  
+   // Mutation: Added +1 to sum
+   public int addPlusOne(int a, int b) {
+       return a + b + 1;
+   }
 }
