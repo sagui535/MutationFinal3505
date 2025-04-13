ca9d111be4836fb2d6658141a43e4c41b4cce633
joulesdominic <54992143+joulesdominic@users.noreply.github.com>
2025-04-12 18:28:01 -0600

    Added Calculator class and tests

---
+++ src/Calculator.java
@@ -0,0 +1,35 @@
+
+public class Calculator {
+	
+    public int add(int a, int b) {
+        return a + b;
+    }
+    
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+    
+    public boolean isPositive(int number) {
+        return number > 0;
+    }
+    
+    public int max(int a, int b) {
+        if (a > b) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+    
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+    
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero");
+        }
+        return a / b;
+    }
+
+}
