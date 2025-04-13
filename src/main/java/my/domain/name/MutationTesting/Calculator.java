7d77e47c223e082a0927796510dd0e3f09b18424
joulesdominic <54992143+joulesdominic@users.noreply.github.com>
2025-04-13 17:16:07 -0600

    Update Calculator.java

--- src/main/java/my/domain/name/MutationTesting/Calculator.java
+++ src/main/java/my/domain/name/MutationTesting/Calculator.java
@@ -1,34 +1,39 @@
 package my.domain.name.MutationTesting;
 
 public class Calculator {
-	
-    public int add(int a, int b) {
-        return a + b;
-    }
     
-    public int subtract(int a, int b) {
+    public int add(int a, int b) {
+        // Mutation: Changed + to -
         return a - b;
     }
     
+    public int subtract(int a, int b) {
+        // Mutation: Swapped operands
+        return b - a;
+    }
+    
     public boolean isPositive(int number) {
-        return number > 0;
+        // Mutation: Changed > to >=
+        return number >= 0;
     }
     
     public int max(int a, int b) {
-        if (a > b) {
+        // Mutation: Changed > to >=
+        if (a >= b) {
             return a;
-        }
-        else {
+        } else {
             return b;
         }
     }
     
     public int multiply(int a, int b) {
-        return a * b;
+        // Mutation: Added 1 to result
+        return a * b + 1;
     }
     
     public int divide(int a, int b) {
-        if (b == 0) {
+        // Mutation: Changed == 0 to <= 0
+        if (b <= 0) {
             throw new IllegalArgumentException("Cannot divide by zero");
         }
         return a / b;
